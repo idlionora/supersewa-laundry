1266af11effa2cf33217b2745420a4e5f6dbf5a9
Nurul Azizah <idlionora@gmail.com>
2023-10-31 19:29:21 +0700

    feat(DummyPage): add text message to redirect user to available pages

--- src/pages/DummyPage.tsx
+++ src/pages/DummyPage.tsx
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router-dom";
+
 function DummyPage() {
 	return (
-		<div>
-			<h1 className="text-3xl">path: various</h1>
+		<div className="w-full py-12 px-5 flex flex-col items-center">
+			<h1 className="text-xl font-medium mb-4 text-center">Maaf, halaman ini belum&nbsp;tersedia.</h1>
+			<p className="text-base text-center max-w-2xl">
+				Silakan klik{' '}
+				<NavLink to="orders" className="underline font-semibold text-theme-blue">
+					/orders
+				</NavLink>{' '}
+				untuk melihat Daftar Pesanan, atau{' '}
+				<NavLink
+					to="new-order"
+					className="underline font-semibold text-theme-blue whitespace-nowrap"
+				>
+					/new-order
+				</NavLink>{' '}
+				untuk melihat halaman Bikin&nbsp;Pesanan.
+			</p>
 		</div>
 	);
 }
